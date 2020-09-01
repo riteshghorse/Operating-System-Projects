9a71db1650f387e0c8f9a22373564030cecc5aeb
riteshghorse <riteshghorse@gmail.com>
2020-09-01 12:19:29 -0400

    pa0 till ebp

--- Getting Acquainted with XINU/csc501-lab0/TMP/printtos.c
+++ Getting Acquainted with XINU/csc501-lab0/TMP/printtos.c
@@ -7,6 +7,7 @@ unsigned long *esp;
 void 
 printtos ()
 {
+	int i = 0;
 	asm ("movl %ebp,ebp");
 	kprintf ("\nvoid printtos()\n");
 	kprintf ("Before[0x%08x]: 0x%08x\n", ebp+2, *(ebp+2));
@@ -15,13 +16,10 @@ printtos ()
 	int a = 1;
 	int b = 2;
 	int c = 3;
-	int d = 4;	
-	++esp;
-	kprintf ("\t\telement[0x%08x]: 0x%08x\n", esp, *esp);
-	++esp;
-	kprintf ("\t\telement[0x%08x]: 0x%08x\n", esp, *esp);
-	++esp;
-	kprintf ("\t\telement[0x%08x]: 0x%08x\n", esp, *esp);
-	++esp;
-	kprintf ("\t\telement[0x%08x]: 0x%08x\n", esp, *esp);
+	int d = 4;
+	for (i = 1; i <= 4; ++i) {
+		if ((esp+i) <= ebp) {
+			kprintf ("\t\telement[0x%08x]: 0x%08x\n", (esp+i), *(esp+i));
+		}
+	}	
 } 
