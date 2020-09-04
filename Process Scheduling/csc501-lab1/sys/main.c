a1eef78baac73bbd25fe5f451178452c22bb5a7e
riteshghorse <riteshghorse@gmail.com>
2020-09-04 12:55:33 -0400

    PA 1 initial

---
+++ Process Scheduling/csc501-lab1/sys/main.c
@@ -0,0 +1,16 @@
+/* user.c - main */
+
+#include <conf.h>
+#include <kernel.h>
+#include <proc.h>
+#include <stdio.h>
+
+/*------------------------------------------------------------------------
+ *  main  --  user main program
+ *------------------------------------------------------------------------
+ */
+int main()
+{
+	kprintf("\n\nHello World, Xinu lives\n\n");
+	return 0;
+}
